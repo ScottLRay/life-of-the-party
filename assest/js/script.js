b4de1a9f39adc1e889649ea8033362f78459dd6f
Drowekorte <drowekorte@gmail.com>
2021-03-11 14:08:21 -0700

    Multiline jokes are working in JS

--- assest/js/script.js
+++ assest/js/script.js
@@ -1,12 +1,20 @@
+
+
 function randomjokes() {
-    var endpoint = `https://v2.jokeapi.dev/joke/Programming,Pun?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&idRange=0-10&amount=1`;
+    var endpoint = `https://v2.jokeapi.dev/joke/Any?type=twopart/Programming,Pun?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&idRange=0-10&amount=1`;
     fetch(endpoint)
         .then((data) => data.json())
         .then(function (joke) {
             var jokes = document.querySelector(".joke-content")
-            jokes.textContent = "Random joke: " + joke.joke
-            console.log(joke);
+            if (joke.joke) {
+                jokes.textContent = "Random joke: " + joke.joke;
+                console.log(joke);
+            } else {
+                jokes.textContent = "Random joke: " + joke.setup + joke.delivery;
+            }
+
         });
+
 }
 
 randomjokes();
